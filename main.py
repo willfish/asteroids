a204b7d0b2518b5627ec8a63116b9ceb7e1f9174
William Fish <william.michael.fish@gmail.com>
2024-12-03 10:00:09 +0000

    Adds initial screen

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print("Screen width: ", SCREEN_WIDTH)
-    print("Screen height: ", SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
 
 if __name__ == "__main__":
     main()
